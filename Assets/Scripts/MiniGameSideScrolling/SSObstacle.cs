5d087699845c35085be35a9129c31fe13a61a6d6
kurusu-nozomi <kokkaseisho225@mail.com>
2021-10-21 13:59:19 +0900

    箱をランダムではなく多量に出しているだけになります。悲惨なことになっているのでこんなところに戻らないといけないかどうかしっかり考えてからリバートしてね。

---
+++ Assets/Scripts/MiniGameSideScrolling/SSObstacle.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+//障害物のクラス。めっちゃFind使ってます。重い死ねってなったら解決法教えて下し亜。
+public class SSObstacle : MonoBehaviour
+{
+    //private const int OBSTRACT_MAX = 5;//妨害箱の最大数
+    //private GameObject[] obstract = new GameObject[5];
+    //private GameObject wall = new GameObject();
+
+    //Start is called before the first frame update
+    private void Start()
+    {
+       // RandomSelectMove();
+        //obstract[1] = GameObject.Find("Obstacle" + 0);
+        //obstract[2] = GameObject.Find("Obstacle" + 1);
+        //obstract[3] = GameObject.Find("Obstacle" + 2);
+        //obstract[4] = GameObject.Find("Obstacle" + 3);
+        //obstract[5] = GameObject.Find("Obstacle" + 4);
+
+        //wall = GameObject.Find("Wall");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Iranaikamo();
+    }
+
+    private void Iranaikamo()
+    {
+        //if (wall.transform.position.z > obstract[1].transform.position.z)
+        //{
+        //    RandomSelectMove();
+        //}
+    }
+
+    //ランダムに箱を最大4つまで障害物として配置します。
+    private void RandomSelectMove()
+    {
+        //int countMax = 4;
+        //int count = 0;
+
+        //System.Random random = new System.Random();
+        //for (int i = 0; i < OBSTRACT_MAX; i++)
+        //{
+        //    int rand = random.Next(random.Next(0, 1));//0or1をランダムで作成
+        //    if (count <= countMax)//箱を出した数が4つ以下なら
+        //    {
+        //        Vector3 pos = obstract[i].transform.position;
+        //        if (rand == 0)
+        //        {
+        //            pos.y = 3;
+
+        //            count++;//カウントを進める
+        //        }
+        //        pos.z += 10.0f;
+        //        obstract[i].transform.position = pos;
+        //    }
+        //}
+    }
+}
--- Assets/Scripts/MiniGameSideScrolling/SSWallMove.cs
+++ Assets/Scripts/MiniGameSideScrolling/SSWallMove.cs
@@ -1,43 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+//ありえんコード汚いです。すみません。
 
 public class SSWallMove : MonoBehaviour
 {
+    //配列とfor文を知らぬ男。
     public int PlayerNumber;
 
-    public static Vector3 wallPosition = new Vector3();//壁の座標
-    const float SPEED = 3.0f;
+    private GameObject player0;
+    private GameObject player1;
+    private GameObject player2;
+    private GameObject player3;
+    private GameObject wall;
+    private const float SPEED = 3.0f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        FindPlayer();
+        Find();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        GameObject wall = GameObject.Find("Wall");
-        wall.transform.Translate(new Vector3(0.0f, 0.0f, 1 * SPEED * Time.deltaTime));
-    }
-    //プレイヤーを殺すなり除外するなりの処理。多分スコアを残すために場所を変えるだけの処理になるんじゃないかな
-    void KillPlayer()
-    {
-        
+        KillPlayer();
+        wall.transform.Translate(new Vector3(0.0f, 0.0f, 1 * SPEED * Time.deltaTime));//壁を前に進める
     }
 
-    string playerName = "Player";
-   
-    void FindPlayer()
+    //プレイヤーを殺すなり除外するなりの処理。死亡エフェクトとともにプレイヤーをカメラ外にぶん投げます。エフェクトの実装は座して待て。
+    private void KillPlayer()
     {
+        //各プレイヤーの座標と死んだときの座標
+        Vector3 pPos0 = player0.transform.position;
+        Vector3 pPos1 = player1.transform.position;
+        Vector3 pPos2 = player2.transform.position;
+        Vector3 pPos3 = player3.transform.position;
+        Vector3 deathPos = new Vector3(0.0f, -1000.0f, 0.0f);
 
-        //_plNumTextHorizontal = PlayerNum.ToString() + "PHorizontal";
-        //名前の付け方やばくて草
-        GameObject[] player = {
-            GameObject.Find(playerName+"0"),
-            GameObject.Find(playerName+"1"),
-            GameObject.Find(playerName+"2"),
-            GameObject.Find(playerName+"3")
-        };
+        if (player0.transform.position.z < wall.transform.position.z)
+        {
+            player0.GetComponent<CharacterController>().Move(deathPos);
+        }
+
+        if (player1.transform.position.z < wall.transform.position.z)
+        {
+            player1.GetComponent<CharacterController>().Move(deathPos);
+        }
+
+        if (player2.transform.position.z < wall.transform.position.z)
+        {
+            player2.GetComponent<CharacterController>().Move(deathPos);
+        }
+
+        if (player3.transform.position.z < wall.transform.position.z)
+        {
+            player3.GetComponent<CharacterController>().Move(deathPos);
+        }
+    }
+
+    private string playerName = "Player";
+
+    private void Find()
+    {
+        player0 = GameObject.Find(playerName + "0");//プレイヤー0
+        player1 = GameObject.Find(playerName + "1");//プレイヤー1
+        player2 = GameObject.Find(playerName + "2");//プレイヤー2
+        player3 = GameObject.Find(playerName + "3");//プレイヤー3
+        wall = GameObject.Find("Wall");             //迫りくる壁。毒ガスっぽい何かになるかもしれない。
     }
 }
