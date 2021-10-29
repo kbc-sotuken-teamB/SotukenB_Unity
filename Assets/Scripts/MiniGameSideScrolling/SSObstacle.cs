531bac8bd09b086721074b3bf14091527459cdbe
kurusu-nozomi <kokkaseisho225@mail.com>
2021-10-29 10:48:31 +0900

    スクロール ・順位未作成 ・未調整

--- Assets/Scripts/MiniGameSideScrolling/SSObstacle.cs
+++ Assets/Scripts/MiniGameSideScrolling/SSObstacle.cs
@@ -1,59 +1,48 @@
-﻿using UnityEngine;
-
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
 //障害物のクラス。めっちゃFind使ってます。重い死ねってなったら解決法教えて下し亜。
 public class SSObstacle : MonoBehaviour
 {
-    private int OBSTRACT_MAX = 25;//妨害箱の最大数
-    private GameObject[] obstract;
-    private GameObject wall;
-
-    //Start is called before the first frame update
-    private void Start()
+    //private bool isCreate = false;//障害物を作るかどうかのフラグ。いらないかもしれない。
+    private int yokoNum = 5;      //障害物を横に並べる数
+    private int yokoMax = 4;      //障害物横の最大数
+    private int tateNum = 10;      //障害物縦の数
+    private List<GameObject> GOList = new List<GameObject>();
+    private Vector3 startVector = new Vector3(0.0f, 0.0f, 0.0f);
+    private const float moveZ = 7.0f;//縦にずらす距離。
+    private const float moveX = 3.0f;//1マスの横のサイズ。
+    public GameObject obstract;
+    private bool Start()
     {
-        obstract = new GameObject[OBSTRACT_MAX];
-
-        wall = GameObject.Find("Wall");
-
-        for (int i = 0; i < OBSTRACT_MAX; i++)          //オブジェクトの最大数廻す
+        for (int i = 0; i < tateNum; i++)//縦
         {
-            int num = i + 1;                            //オブジェクトの名前を1番から作ったバカがいると聞いて。
-            string myName = "Obstacle" + num.ToString();//オブジェクトの名前完成
-            obstract[i] = GameObject.Find(myName);      //Find関数使ってるだけ
-        }
-        RandomSelectMove();                             //ランダムに障害物を上にずらす関数。ランダム生成でよくね。
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        //アップデートに何もないってま！？
-    }
-
-    //ランダムに箱を最大5×4まで障害物として配置します。数は変わります。
-    private void RandomSelectMove()
-    {
-        int countMax = 4;
-        int count2 = 0;
-        System.Random random = new System.Random();
-        for (int i = 0; i < 5; i++)//障害物縦列
-        {
-            count2++;
-            int count = 0;
-            for (int j = 0; j < 5; j++)//障害物横列
+            startVector.z += moveZ;//初期座標のままだとプレイヤーと被るので少し前に持っていく
+            startVector.x = -10.5f;//初期座標を先のコードでずらしてるから戻す。
+            int createCount = 0;
+            for (int j = 0; j < yokoNum; j++)//横
             {
-                int rand = random.Next(random.Next(0, 2));              //0か1をランダムで選ぶ
-                if (count <= countMax)                                  //箱を出した数が4つ以下なら
+                if (createCount < yokoMax && BoolRandom() == true)
                 {
-                    Vector3 pos = obstract[count2].transform.position;  //どうやらC#では直接構造体の変数を弄れないと聞いて。
-                    if (rand == 0)
-                    {
-                        pos.y = 0;                                      //Y=0座標です。0だと妨害される高さなんで。
-
-                        count++;                                        //カウントを進める
-                    }
-                    obstract[count2].transform.position = pos;          //座標の更新。
+                    obstract =　(GameObject)Instantiate(obstract, startVector, Quaternion.identity);//座標と回転を設定してインスタンスを生成
+                    createCount++;
                 }
+                startVector.x += moveX;//1マス分横にずらす
             }
         }
+        return true;
+    }
+
+    private void Update()
+    {
+    }
+
+    /// <summary>
+    /// bool型の乱数
+    /// </summary>
+    /// <returns>bool型の乱数</returns>
+    private bool BoolRandom()
+    {
+        return Random.Range(0, 2) == 0;
     }
 }
--- Assets/Scripts/MiniGameSideScrolling/SSWallMove.cs
+++ Assets/Scripts/MiniGameSideScrolling/SSWallMove.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 
-//ありえんコード汚いです。すみません。
-
 public class SSWallMove : MonoBehaviour
 {
     //配列とfor文を知らぬ男。
     public int PlayerNumber;
+
+    private GameObject[] player;
 
     private GameObject player0;
     private GameObject player1;
     private GameObject player2;
     private GameObject player3;
     private GameObject wall;
+    private bool[] deathFlag = new bool[5];
     private const float SPEED = 3.0f;
 
     // Start is called before the first frame update
@@ -31,37 +32,39 @@ public class SSWallMove : MonoBehaviour
     private void KillPlayer()
     {
         //各プレイヤーの座標と死んだときの座標
-        Vector3 pPos0 = player0.transform.position;
-        Vector3 pPos1 = player1.transform.position;
-        Vector3 pPos2 = player2.transform.position;
-        Vector3 pPos3 = player3.transform.position;
-        Vector3 deathPos = new Vector3(0.0f, -1000.0f, 0.0f);
+        Vector3 deathPos = new Vector3(0.0f, -500.0f, 0.0f);
+        float zSize = 2.0f;
 
-        if (player0.transform.position.z < wall.transform.position.z)
+        if (player0.transform.position.z < wall.transform.position.z + zSize)
         {
             player0.GetComponent<CharacterController>().Move(deathPos);
         }
 
-        if (player1.transform.position.z < wall.transform.position.z)
+        if (player1.transform.position.z < wall.transform.position.z + zSize)
         {
             player1.GetComponent<CharacterController>().Move(deathPos);
         }
 
-        if (player2.transform.position.z < wall.transform.position.z)
+        if (player2.transform.position.z < wall.transform.position.z + zSize)
         {
             player2.GetComponent<CharacterController>().Move(deathPos);
         }
 
-        if (player3.transform.position.z < wall.transform.position.z)
+        if (player3.transform.position.z < wall.transform.position.z + zSize)
         {
             player3.GetComponent<CharacterController>().Move(deathPos);
         }
     }
 
-    private string playerName = "Player";
-
     private void Find()
     {
+        string playerName = "Player";
+        player = new GameObject[5];
+        for (int i = 0; i < 4; i++)
+        {
+            string num = i.ToString();
+            player[i] = GameObject.Find(playerName + num);
+        }
         player0 = GameObject.Find(playerName + "0");//プレイヤー0
         player1 = GameObject.Find(playerName + "1");//プレイヤー1
         player2 = GameObject.Find(playerName + "2");//プレイヤー2
