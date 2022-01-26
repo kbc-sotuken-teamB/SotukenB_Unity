21fef123bbc78ce6c6e2659fa50fecee3b76b2e2
TanakaMikiya <kbc19b13@stu.kawahara.ac.jp>
2022-01-26 12:11:38 +0900

    animatorの更新

---
+++ Assets/Scripts/MainGame/PlayerAnim.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+	Animation anim;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		anim = this.gameObject.GetComponent<Animation>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.Space))
+		{
+
+			anim.Play();
+
+
+
+		}
+	}
+}
+/*
+ 
+	Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.gameObject.GetComponent<Animation> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			anim.Play ();
+
+
+
+		}
+
+		
+	}
+}
+ */
